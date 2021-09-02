b6216d468c82eec850021e702e340faa4457879d
Snehil <snehilsneh888@gmail.com>
2021-09-02 14:53:45 +0530

    Added navbar layout and nprogress

---
+++ components/Layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Menu,Container,Icon } from "semantic-ui-react";
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+
+function Navbar() {
+  const router = useRouter()
+
+  const isActive=route=>router.pathname===route
+
+
+  return (
+    <Menu fixed="top" fluid style={{ backgroundColor: "#264348" }}>
+      <Container text >
+        <Link href="/login">
+          <Menu.Item header active={isActive('/login')} style={{ color: "white" }}>
+            <Icon size="large" name="sign in" />
+            <span >Login</span>
+          </Menu.Item>
+        </Link>
+
+        <Link href="/signup">
+          <Menu.Item header active={isActive('/signup')} style={{ color: "white" }}>
+            <Icon size="large" name="signup" />
+            <span>Signup</span>
+          </Menu.Item>
+        </Link>
+      </Container>
+    </Menu>
+  );
+}
+
+export default Navbar;
