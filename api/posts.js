503e9a4150529330e97a5fd98ecea683ab025440
Snehil <snehilsneh888@gmail.com>
2021-09-18 19:09:29 +0530

    Api for creating and deleting a comment

--- api/posts.js
+++ api/posts.js
@@ -226,3 +226,91 @@ router.get("/like/:postId", authMiddleware, async (req, res) => {
     return res.status(500).send(`Server error`);
   }
 });
+
+
+
+
+
+// CREATE A COMMENT
+
+router.post("/comment/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    const { text } = req.body;
+
+    if (text.length < 1)
+      return res.status(401).send("Comment should be atleast 1 character");
+
+    const post = await PostModel.findById(postId);
+
+    if (!post) return res.status(404).send("Post not found");
+
+    const newComment = {
+      _id: uuid(),
+      text,
+      user: req.userId,
+      date: Date.now()
+    };
+
+    await post.comments.unshift(newComment);
+    await post.save();
+
+    return res.status(200).json(newComment._id);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+
+
+
+
+// DELETE A COMMENT
+
+router.delete("/:postId/:commentId", authMiddleware, async (req, res) => {
+  try {
+    const { postId, commentId } = req.params;
+    const { userId } = req;
+
+    const post = await PostModel.findById(postId);
+    if (!post) return res.status(404).send("Post not found");
+
+    const comment = post.comments.find(comment => comment._id === commentId);
+    if (!comment) {
+      return res.status(404).send("No Comment found");
+    }
+
+    const user = await UserModel.findById(userId);
+
+    const deleteComment = async () => {
+      const indexOf = post.comments.map(comment => comment._id).indexOf(commentId);
+
+      await post.comments.splice(indexOf, 1);
+
+      await post.save();
+
+      return res.status(200).send("Deleted Successfully");
+    };
+
+    if (comment.user.toString() !== userId) {
+      if (user.role === "root") {
+        await deleteComment();
+      } else {
+        return res.status(401).send("Unauthorized");
+      }
+    }
+
+    await deleteComment();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+
+
+
+
+module.exports = router;
