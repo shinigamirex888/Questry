d8ccf38bcafa4abcec9671b8815b8f3d7603e007
Snehil <snehilsneh888@gmail.com>
2021-09-18 18:51:04 +0530

    Api for like unlike and get all like of a post

--- api/posts.js
+++ api/posts.js
@@ -135,3 +135,94 @@ router.delete("/:postId", authMiddleware, async (req, res) => {
     return res.status(500).send(`Server error`);
   }
 });
+
+
+
+
+
+// LIKE A POST
+
+router.post("/like/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId } = req;
+
+    const post = await PostModel.findById(postId);
+    if (!post) {
+      return res.status(404).send("No Post found");
+    }
+
+    const isLiked =
+      post.likes.filter(like => like.user.toString() === userId).length > 0;
+
+    if (isLiked) {
+      return res.status(401).send("Post already liked");
+    }
+
+    await post.likes.unshift({ user: userId });
+    await post.save();
+
+    return res.status(200).send("Post liked");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+
+
+
+
+// UNLIKE A POST
+
+router.put("/unlike/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId } = req;
+
+    const post = await PostModel.findById(postId);
+    if (!post) {
+      return res.status(404).send("No Post found");
+    }
+
+    const isLiked =
+      post.likes.filter(like => like.user.toString() === userId).length === 0;
+
+    if (isLiked) {
+      return res.status(401).send("Post not liked before");
+    }
+
+    const index = post.likes.map(like => like.user.toString()).indexOf(userId);
+
+    await post.likes.splice(index, 1);
+
+    await post.save();
+
+    return res.status(200).send("Post Unliked");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+
+
+
+
+// GET ALL LIKES OF A POST 
+
+router.get("/like/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await PostModel.findById(postId).populate("likes.user");
+    if (!post) {
+      return res.status(404).send("No Post found");
+    }
+
+    return res.status(200).json(post.likes);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
