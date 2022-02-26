89a7547c108f65a511b5e536f25e721f76d9f3a4
Snehil <snehilsneh888@gmail.com>
2022-02-26 20:12:07 +0530

    adding image

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,11 +4,11 @@ const uploadPic = async media => {
   try {
     const form = new FormData();
     form.append("file", media);
-    form.append("upload_preset", "Questry");
-    form.append("cloud_name", "divuvgfkp");
+    form.append("upload_preset", "social_media_app");
+    form.append("cloud_name", "indersingh");
 
     const res = await axios.post(process.env.CLOUDINARY_URL, form);
-    return res.data.url;
+    return res.data.secure_url;
   } catch (error) {
     return;
   }
