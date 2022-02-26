9860c36a9196901f2799227fbe09912840a56aa0
Snehil <snehilsneh888@gmail.com>
2022-02-26 20:27:51 +0530

    Cloudinary bug

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,11 +4,11 @@ const uploadPic = async media => {
   try {
     const form = new FormData();
     form.append("file", media);
-    form.append("upload_preset", "social_media_app");
-    form.append("cloud_name", "indersingh");
+    form.append("upload_preset", "Questry");
+    form.append("cloud_name", "divuvgfkp");
 
     const res = await axios.post(process.env.CLOUDINARY_URL, form);
-    return res.data.secure_url;
+    return res.data.url;
   } catch (error) {
     return;
   }
